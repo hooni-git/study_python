e5f5e1ed875b7950225c38ecf1f7cd27a652f11a
codehoon <codehoon650@gmail.com>
2022-11-04 23:04:14 +0900

    1158_success

---
+++ ArrayList_LinkedList/1158.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def insert(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def get_node(self, index):
+        node = self.head
+        count = 0
+        while count < index:
+            count += 1
+            node = node.next
+        return node
+    
+    def delete(self, index):
+        if index == 0:
+            del_node = self.head.data
+            self.head = self.head.next
+            return del_node
+        node = self.get_node(index - 1)
+        del_node = node.next.data
+        node.next = node.next.next
+        return del_node
+
+n, k = map(int, input().split())
+man = LinkedList(1)
+
+for i in range(2, n+1):
+    man.insert(i)
+answer = []
+idx = k - 1
+
+while man.head is not None:
+    idx %= n
+    answer.append(man.delete(idx))
+    idx += (k-1)
+    n -= 1
+print('<', end='')
+for i in range(len(answer) - 1):
+    print(answer[i], end=', ')
+print(answer[len(answer) - 1], end='')
+print('>')
